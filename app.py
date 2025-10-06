bddfcd778e3ecb1cc024e7992018e9a9421ecdf3
Abuqataada <99135924+Abuqataada@users.noreply.github.com>
2025-10-06 10:41:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -807,6 +807,8 @@ with app.app_context():
     except Exception as e:
         print(f"ERROR: Error creating tables: {e}")
 
+handler = app
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
